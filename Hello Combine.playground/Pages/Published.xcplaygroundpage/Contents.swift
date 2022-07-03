ccfdbf17efe629dcaa72e1743e8bbc92aac4601c
Youngjae Choi <cdw0807@naver.com>
2022-07-03 21:16:40 +0900

    Published

--- Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
@@ -4,6 +4,25 @@ import Foundation
 import UIKit
 import Combine
 
+final class SomeViewModel {
+    @Published var name: String = "Jack"
+    var age: Int = 20
+}
+
+final class Label {
+    var text: String = ""
+}
+
+let label = Label()
+let vm = SomeViewModel()
+print("text: \(label.text)")
+
+// 퍼블리셔($name)의 데이터를 label에 있는 text에 할당한다.
+vm.$name.assign(to: \.text, on: label)
+print("text: \(label.text)")
+
+vm.name = "Jason"
+print("text: \(label.text)")
 
 
 

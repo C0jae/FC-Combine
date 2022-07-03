a8a99dc45553bd8bbded334e10310ed72f07f062
Youngjae Choi <cdw0807@naver.com>
2022-07-03 21:33:36 +0900

    Foundation and Combine

--- Hello Combine.playground/Pages/Foundation and Combine.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Foundation and Combine.xcplaygroundpage/Contents.swift
@@ -7,26 +7,48 @@ import UIKit
 
 
 // URLSessionTask Publisher and JSON Decoding Operator
+struct SomeDecodable: Decodable { }
 
-URLSession.shared.dataTaskPublisher(for: URL(string: "www.google.com")!)
+URLSession.shared.dataTaskPublisher(for: URL(string: "https://www.google.com")!)
     .map { data, response in
         return data
     }
-    .decode(type: <#T##Decodable.Protocol#>, decoder: <#T##TopLevelDecoder#>)
-
+    .decode(type: SomeDecodable.self, decoder: JSONDecoder())
 
 // Notifications
+let center = NotificationCenter.default
+let noti = Notification.Name("MyNoti")
+let notiPublisher = center.publisher(for: noti, object: nil)
+let subscription1 = notiPublisher.sink { _ in
+    print("Noti Received")
+}
 
-
-
-
+center.post(name: noti, object: nil)
+subscription1.cancel()
 
 // KeyPath binding to NSObject instances
+let ageLabel = UILabel()
+print("text: \(ageLabel.text)")
 
-
+Just(28)
+    .map { "Age is \($0)" }
+    .assign(to: \.text, on: ageLabel)
+print("text: \(ageLabel.text)")
 
 // Timer
 // autoconnect 를 이용하면 subscribe 되면 바로 시작함
+let timerPublisher = Timer
+    .publish(every: 1, on: .main, in: .common)
+    .autoconnect()
 
+// 매 1초마다 출력
+let subscription2 = timerPublisher.sink { time in
+    print("time: \(time)")
+}
+
+// 5초뒤에 구독취소(출력 중지)
+DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+    subscription2.cancel()
+}
 
 //: [Next](@next)

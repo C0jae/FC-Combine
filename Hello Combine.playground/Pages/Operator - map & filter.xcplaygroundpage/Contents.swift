1cdfe572edf8ba5b5a0ce6ffd52310d44e2ca4a7
Youngjae Choi <cdw0807@naver.com>
2022-07-03 21:49:01 +0900

    Operator

--- Hello Combine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
@@ -4,9 +4,32 @@ import Foundation
 import Combine
 
 // Transform - Map
+let numPublisher = PassthroughSubject<Int, Never>()
+let subscription1 = numPublisher
+    .map { $0 * 2 }
+    .sink { value in
+        print("Transformed Value: \(value)")
+    }
 
+numPublisher.send(10)
+numPublisher.send(20)
+numPublisher.send(30)
+subscription1.cancel()
 
 // Filter
+let stringPublisher = PassthroughSubject<String, Never>()
+let subscription2 = stringPublisher
+    .filter { $0.contains("a") }    // a가 포함된 데이터만 분류
+    .sink { value in
+        print("Filter Value: \(value)")
+    }
+
+stringPublisher.send("abc")
+stringPublisher.send("Jack")
+stringPublisher.send("John")
+stringPublisher.send("Jenny")
+stringPublisher.send("Lisa")
+subscription2.cancel()
 
 
 //: [Next](@next)
